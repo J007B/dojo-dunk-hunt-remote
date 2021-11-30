86d66b2726333370f5b77122414445c163d14171
Jean-François Morin <jeanfrancois.morin@wildcodeschool.com>
2021-11-30 09:20:29 +0100

    remote version

---
+++ script2.js
@@ -0,0 +1,29 @@
+const duck = document.getElementById('duck');
+const bullets = document.getElementsByClassName('bullet')
+const stepX = 100;
+const stepY = 50;
+const maxY = 450;
+const maxX = 1000;
+//  call the duck element 'duck'
+//Make the duck move with the stepX or stepY increment
+//When the duck moves, the duck element has only one class name
+//.duck-down or .duck-right or .duck-up or duck-left among its direction
+
+
+
+
+//prevent the duck from going out of the screen
+
+
+
+
+
+//call the shoot function (already implemented)  when duck is shot
+
+
+
+
+
+//Manage the Game Over Case by using bulletCount variable
+
+
